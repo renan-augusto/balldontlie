14dc50bef4796541a2b42f729f8ebaaf7a9e2391
renan-augusto <renan_acm1992@hotmail.com>
2023-12-26 10:31:38 -0300

    fix: imports fixed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ICommonType } from 'src/app/models/common.models';
+import { ICommonType } from 'src/app/models/common.model';
 
 @Component({
   selector: 'app-home',
