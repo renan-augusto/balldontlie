f0666358ce9aa9ab099fe703d52b38213b2be9b2
renan-augusto <renan_acm1992@hotmail.com>
2023-12-26 10:31:13 -0300

    fix: common model fixed

--- src/app/models/common.model.ts
+++ src/app/models/common.model.ts
@@ -3,7 +3,7 @@ export interface ResultWapper<T> {
     meta: {
         total_pages: number,
         current_page: number,
-        next_page: number,
+        next_page?: number,
         per_page: number,
         total_count: number
     };
