308af3580a38ea22848b74201d6738ba2fc66afb
renan-augusto <renan_acm1992@hotmail.com>
2024-01-02 10:48:56 -0300

    tests: mocked options value test

--- src/app/shared/select/select.component.spec.ts
+++ src/app/shared/select/select.component.spec.ts
@@ -33,7 +33,19 @@ describe('SelectComponent', () => {
   });
 
   it('should set options via @input', () => {
-    const options: ICommonType[] = [];
+    const options: ICommonType[] = [
+      { label: 'Albert Einstein', value: '3' },
+      { label: 'Marie Curie', value: '4' },
+      { label: 'Isaac Newton', value: '5' },
+      { label: 'Ada Lovelace', value: '6' },
+      { label: 'Nikola Tesla', value: '7' },
+      { label: 'Leonardo da Vinci', value: '8' },
+      { label: 'Jane Austen', value: '9' },
+      { label: 'Wolfgang Mozart', value: '10' },
+      { label: 'Cleópatra', value: '11' },
+      { label: 'Vincent van Gogh', value: '12' },
+      { label: 'Bill Gates', value: '13' }
+    ];
     component.options = options;
 
     fixture.detectChanges();
