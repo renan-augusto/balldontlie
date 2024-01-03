d64dd84d1e528342f22b9bc8214200919298f42d
renan-augusto <renan_acm1992@hotmail.com>
2024-01-03 07:14:04 -0300

    test: services testbed configurations

--- src/app/core/players.service.spec.ts
+++ src/app/core/players.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PlayersService } from './players.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PlayersService', () => {
   let service: PlayersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(PlayersService);
   });
 
--- src/app/core/teams.service.spec.ts
+++ src/app/core/teams.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TeamsService } from './teams.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('TeamsService', () => {
   let service: TeamsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(TeamsService);
   });
 
