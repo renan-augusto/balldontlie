8f9a948347f974e06213dd668e9e9db43b9f7740
renan-augusto <renan_acm1992@hotmail.com>
2024-01-19 07:03:07 -0300

    construction: franchises pages started

---
+++ src/app/pages/franchises/franchises.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-franchises',
+  templateUrl: './franchises.component.html',
+  styleUrls: ['./franchises.component.css']
+})
+export class FranchisesComponent {
+
+}
