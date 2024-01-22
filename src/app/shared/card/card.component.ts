84f833d96e6ddfed709c28e9d5315b9051bda151
renan-augusto <renan_acm1992@hotmail.com>
2024-01-22 07:14:41 -0300

    construction(card) - Adicionadas propriedades no card component

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  @Input() height: number = 300;
+  @Input() disable: boolean = false;
+  @Input() title!: string;
+  // @Input() width: string = '200px';
 
+  private _width: string = '200';
+
+  @Input()
+  set width(value: string) {
+    this._width = value.endsWith('px') ? value : value = value + 'px';
+  }
+
+  get width() {
+    return this._width;
+  }
 }
