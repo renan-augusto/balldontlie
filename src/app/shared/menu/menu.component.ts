1cff78c3fb4a5e521d7b36fd8f41ec7fa81d561d
renan-augusto <renan_acm1992@hotmail.com>
2024-01-23 07:00:45 -0300

    feat(menu) - adicionada opcao de navegacao para home

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -12,6 +12,7 @@ export class MenuComponent {
   menuItemSelected!: string;
 
   menuItems: Array<PoMenuItem> = [
+    {label: 'Home', icon: 'po-icon po-icon-home', shortLabel: 'Home', action: () => this.navigateTo('')},
     {label: 'Jogadores', icon: 'po-icon-star', shortLabel: 'Jogadores', action: () => this.navigateTo('players')},
     {label: 'Franquias', icon: 'po-icon po-icon-world', shortLabel: 'Franquias', action: () => this.navigateTo('franchises')}
   ]
