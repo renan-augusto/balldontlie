f70cf9592f024de31096c39d859107a1da455bcb
renan-augusto <renan_acm1992@hotmail.com>
2024-03-14 06:58:26 -0300

    chore - url and services adjustments for new api adress

--- src/app/core/teams.service.ts
+++ src/app/core/teams.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ResultWapper } from '../models/common.model';
+import { IResultWapperGeneral, ResultWapper } from '../models/common.model';
 import { Teams } from '../models/teams.model';
 import { environment } from 'src/environments/environment';
 
@@ -11,18 +11,23 @@ export class TeamsService {
 
   constructor(private http: HttpClient) { }
 
-  private baseUrl = environment.api
+  private baseUrl = environment.api;
+
+  private header: HttpHeaders = new HttpHeaders({
+    'Authorization': environment.header.apiKey
+  })
+
 
   getTeams() {
-    return this.http.get<ResultWapper<Teams>>(`${this.baseUrl}teams`);
+    return this.http.get<IResultWapperGeneral<Teams>>(`${this.baseUrl}teams`, {headers: this.header});
   }
 
   getPagination(page: any, perPage: any) {
-    return this.http.get<ResultWapper<Teams>>(`${this.baseUrl}teams/?page=${page}&per_page=${perPage}`);
+    return this.http.get<ResultWapper<Teams>>(`${this.baseUrl}teams/?next_cursor=${page}&per_page=${perPage}`, {headers: this.header});
   }
 
   getTeamById(id: any) {
-    return this.http.get<Teams>(`${this.baseUrl}teams/${id}`);
+    return this.http.get<Teams>(`${this.baseUrl}teams/${id}`, {headers: this.header});
   }
 
 }
